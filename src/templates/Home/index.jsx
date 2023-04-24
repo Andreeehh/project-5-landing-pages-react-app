df0e0cdb028434bbca0e2aa0ed0b89fa3c6f47c2
André Carvalho <andre.vrlcarvalho@hotmail.com>
2023-04-24 15:13:33 -0300

    ajustes finais

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,29 +1,31 @@
-import { useEffect } from 'react';
-import { useRef } from 'react';
-import { useState } from 'react';
-import * as Styled from './styles';
-
-import { Heading } from '../../components/Heading';
-import { GridTwoColumns } from '../../components/GridTwoColumn';
+import { useEffect, useRef, useState } from 'react';
 
 import { mapData } from '../../api/map-data';
 
-import { mockBase } from '../Base/mock';
-import { Base } from '../Base';
-import { PageNotFound } from '../PageNotFound';
-import { Loading } from '../Loading';
+import { GridTwoColumns } from '../../components/GridTwoColumns';
 import { GridContent } from '../../components/GridContent';
 import { GridText } from '../../components/GridText';
 import { GridImage } from '../../components/GridImage';
 
+import { Base } from '../Base';
+import { PageNotFound } from '../PageNotFound';
+import { Loading } from '../Loading';
+import { useLocation } from 'react-router-dom';
+
+import config from '../../config';
+
 function Home() {
   const [data, setData] = useState([]);
   const isMounted = useRef(true);
+  const location = useLocation();
 
   useEffect(() => {
     const load = async () => {
+      const pathName = location.pathname.replace(/[^a-z0-9-_]/gi, '');
+      const slug = pathName ? pathName : config.defaultSlug;
+
       try {
-        const data = await fetch('http://localhost:1337/api/pages/?filters[slug]=olha-so-a-minha-pagina&populate=deep');
+        const data = await fetch(`http://localhost:1337/api/pages/?filters[slug]=${slug}&populate=deep`);
         const json = await data.json();
         const { attributes } = json.data[0];
         const pageData = mapData([attributes]);
@@ -40,14 +42,28 @@ function Home() {
     return () => {
       isMounted.current = false;
     };
-  }, []);
+  }, [location]);
+
+  useEffect(() => {
+    if (data === undefined) {
+      document.title = `Página não encontrada | ${config.siteName}`;
+    }
+
+    if (data && !data.slug) {
+      document.title = `Carregando... | ${config.siteName}`;
+    }
+
+    if (data && data.title) {
+      document.title = `${data.title} | ${config.siteName}`;
+    }
+  }, [data]);
 
   if (data === undefined) {
     return <PageNotFound />;
   }
 
   if (data && !data.slug) {
-    return <Loading></Loading>;
+    return <Loading />;
   }
 
   const { menu, sections, footerHtml, slug } = data;
@@ -58,6 +74,7 @@ function Home() {
       {sections.map((section, index) => {
         const { component } = section;
         const key = `${slug}-${index}`;
+
         if (component === 'section.section-two-columns') {
           return <GridTwoColumns key={key} {...section} />;
         }
--- src/templates/PageNotFound/index.jsx
+++ src/templates/PageNotFound/index.jsx
@@ -1,11 +1,7 @@
-import P from 'prop-types';
 import { GridContent } from '../../components/GridContent';
 
 export const PageNotFound = () => {
   return (
-    <GridContent
-      title="Error 404"
-      html='<p>A página que você busca não foi encontrada. <a href="/">Clique para voltar</a></p>'
-    />
+    <GridContent title="Error 404" html="A página que você busca não foi encontrada. [Clique aqui para voltar](/)" />
   );
 };
