0621a852845152b5a63924950c53d68c7c06fc41
André Carvalho <andre.vrlcarvalho@hotmail.com>
2023-05-22 13:25:38 -0300

    ajustes config

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 export default {
-  url: 'http://localhost:1337/api/pages/?filters[slug]=',
+  url: 'https://strapiv2-bfvw.onrender.com/api/pages/?filters[slug]=',
   siteName: 'Landing pages',
   defaultSlug: 'olha-so-a-minha-pagina',
 };
